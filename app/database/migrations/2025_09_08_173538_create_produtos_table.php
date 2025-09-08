c93ef551eadfb25e0c853af87b6965b4f4db027f
Victor <victorfarian@gmail.com>
2025-09-08 15:12:17 -0300

    criando migration de produtos

---
+++ app/database/migrations/2025_09_08_173538_create_produtos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->decimal('valorDeCompra', 10, 2);
+            $table->decimal('valorDeVenda', 10, 2);
+            $table->foreignId('fornecedor_id')->constrained('fornecedores');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produtos');
+    }
+};
