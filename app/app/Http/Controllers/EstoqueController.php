1cc5c2de1e5c35b3d9fa0c28dfcc4fa5ead930c1
Victor <victorfarian@gmail.com>
2025-09-12 15:23:08 -0300

    Criando a base do controller de estoque

---
+++ app/app/Http/Controllers/EstoqueController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Estoque;
+use Illuminate\Http\Request;
+
+class EstoqueController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Estoque $estoque)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Estoque $estoque)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Estoque $estoque)
+    {
+        //
+    }
+}
--- app/app/Http/Requests/StoreProdutoRequest.php
+++ app/app/Http/Requests/StoreProdutoRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreProdutoRequest extends FormRequest
+class StoreProdutoRequest extends FormRequest//TODO fazer um de update
 {
     /**
      * Determine if the user is authorized to make this request.
