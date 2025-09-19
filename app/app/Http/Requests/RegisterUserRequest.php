7881e11ed3298f0d5cfc664b8f7f1d5bb94deb96
Victor <victorfarian@gmail.com>
2025-09-19 15:52:46 -0300

    Adicionando arquivos de autenticação

---
+++ app/app/Http/Requests/RegisterUserRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class RegisterUserRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:users,email',
+            'password' => 'required|string|min:6|confirmed',
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        $errors = $validator->errors();
+
+        throw new HttpResponseException(
+            response()->json([
+                'message' => 'Erro de validação',
+                'errors' => $errors
+            ], 422)
+        );
+    }
+}
