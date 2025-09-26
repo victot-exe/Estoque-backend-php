32e3630f553efbab69b2565e28cfb1f51195dbc1
Victor <victorfarian@gmail.com>
2025-09-26 14:22:45 -0300

    adicionando o endpoint de loggout

--- app/routes/api.php
+++ app/routes/api.php
@@ -16,6 +16,8 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function(){
+
+    Route::post('/logout', [AuthController::class, 'logout']);
     
     Route::get('fornecedores/all-informations', [FornecedorController::class, 'showAllInformations']);
     Route::apiResource('fornecedores', FornecedorController::class)
